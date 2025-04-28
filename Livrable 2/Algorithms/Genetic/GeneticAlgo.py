d44d8e8636a29be51b710c7f4ee0607946bfb106
BDF172 <bdf172@bdfhome.local>
2025-04-28 16:10:04 +0200

    Genetic algorithm done

--- Livrable 2/Algorithms/Genetic/GeneticAlgo.py
+++ Livrable 2/Algorithms/Genetic/GeneticAlgo.py
@@ -3,12 +3,9 @@ import numpy as np
 import networkx as nx
 from Algorithms.AbstractAlgo import AbstractAlgo
 
-class GeneticAlgo:
-    def __init__(self, graph, name = None, iterations = 100, trucks = 1, solutions_in_parallel = 10):
-        self.graph = graph
-        self.name = name
-        self.iterations = iterations
-        self.trucks = trucks
+class GeneticAlgo(AbstractAlgo):
+    def __init__(self, graph, name = None, num_vehicles = 1, solutions_in_parallel = 10, min_iterations = 0, max_iterations = 100, convergence_threshold = 5):
+        super().__init__(graph=graph, name=name, num_vehicles=num_vehicles, min_iterations=min_iterations, max_iterations=max_iterations, convergence_threshold=convergence_threshold)
         self.solutions_in_parallel = solutions_in_parallel
         self.get_distance_matrix()
 
@@ -21,50 +18,48 @@ class GeneticAlgo:
 
 
     def get_random_solutions(self):
-        # Generate a list(solution) of lists(containing the destination per truck)
-        # Each truck will have a random path
+        # Generate a list(solution) of lists(containing the destination per vehicle)
+        # Each vehicle will have a random path
         # The path will be a random permutation of the nodes
 
         solutions = []
 
-        for i in range(self.solutions_in_parallel):
+        for solution_index in range(self.solutions_in_parallel):
             nodes = list(self.graph.nodes)[1:]
-            solution = [[] for i in range(self.trucks)]
+            solution = [[] for i in range(self.num_vehicles)]
             while len(nodes) > 0:
-                for i in range(self.trucks):
+                for i in range(self.num_vehicles):
                     if len(nodes) == 0:
                         break
                     node = random.choice(nodes)
                     solution[i].append(node)
                     nodes.remove(node)
                 
-            for i in range(len(solution)):
-                if len(solution[i]) == 0:
+            for j in range(len(solution)):
+                if len(solution[j]) == 0:
                     continue
-                solution[i].insert(0, list(self.graph.nodes)[0])
-                solution[i].append(list(self.graph.nodes)[0])
+                solution[j].insert(0, list(self.graph.nodes)[0])
+                solution[j].append(list(self.graph.nodes)[0])
             
-                solutions.append(solution)
+            solutions.append(solution)
         
         return solutions
     
-    def get_distance_per_truck(self, solution):
-        # Calculate the distance for each truck
-        distances = []
+    def get_distance_per_vehicle(self, solution):
+        # Calculate the distance for each vehicle
+        distances = 0
         for i in range(len(solution)):
-            distance = 0
             for j in range(len(solution[i])):
                 if j == len(solution[i]) - 1:
                     break
-                distance += nx.shortest_path_length(self.graph, source=solution[i][j], target=solution[i][j+1], weight="weight")
-            distances.append(distance)
+                distances += nx.shortest_path_length(self.graph, source=solution[i][j], target=solution[i][j+1], weight="weight")
         return distances
     
     def get_total_distance(self, solution):
         # Calculate the total distance of the solution
         total_distance = 0
         for i in range(len(solution)):
-            total_distance += self.get_distance_per_truck(solution)[i]
+            total_distance += self.get_distance_per_vehicle(solution[i])
         return total_distance
     
     def mutate(self, solution):
@@ -92,18 +87,18 @@ class GeneticAlgo:
         # Run the genetic algorithm
         # Generate a random solution
         solutions = self.get_random_solutions()
+        self.distance_history = [] #TODO
         
         
-        for i in range(self.iterations):
+        for i in range(self.max_iterations):
             # Sort solutions by their total distance
             solutions = sorted(solutions, key=lambda s: self.get_total_distance(s))
-            solutions_distances = [self.get_total_distance(s) for s in solutions]
             solutions = solutions[:len(solutions)//2] 
             solutions_distances = [self.get_total_distance(s) for s in solutions]
+            self.distance_history.append(solutions_distances[0])
 
             # Create new solutions by combining the best solutions
             solutions = self.mutate(solutions)
 
-        value = sorted(solutions, key=lambda s: self.get_total_distance(s))[0]
-        value2 = self.get_total_distance(value)
-        return sorted(solutions, key=lambda s: self.get_total_distance(s))[0]
+        paths = sorted(solutions, key=lambda s: self.get_total_distance(s))[0]
+        self.paths = sorted(solutions, key=lambda s: self.get_total_distance(s))[0]
