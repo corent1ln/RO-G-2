bb8a6aff4980de929d6d9b68a3ecc8a545987e82
LeoVtr76 <62765434+LeoVtr76@users.noreply.github.com>
2025-04-29 01:05:30 +0200

    feat: update genetic algo to get somes statistics data and minimse the stard deviations for many vehicles

--- Livrable 2/Algorithms/Genetic/GeneticAlgo.py
+++ Livrable 2/Algorithms/Genetic/GeneticAlgo.py
@@ -1,19 +1,12 @@
 import random
-import networkx as nx
+import time
+import numpy as np
 from Algorithms.AbstractAlgo import AbstractAlgo
 
 class GeneticAlgo(AbstractAlgo):
     def __init__(self, graph, name = None, num_vehicles = 5, solutions_in_parallel = 10, min_iterations = 0, max_iterations = 100, convergence_threshold = 5):
         super().__init__(graph=graph, name=name, num_vehicles=num_vehicles, min_iterations=min_iterations, max_iterations=max_iterations, convergence_threshold=convergence_threshold)
         self.solutions_in_parallel = solutions_in_parallel
-        self.get_distance_matrix()
-
-    def get_distance_matrix(self):
-        self.distance_matrix = {}
-        for i in range(len(list(self.graph.nodes))):
-            self.distance_matrix[list(self.graph.nodes)[i]] = {}
-            for j in range(len(list(self.graph.nodes))):
-                self.distance_matrix[list(self.graph.nodes)[i]][list(self.graph.nodes)[j]] = nx.shortest_path_length(self.graph, source=list(self.graph.nodes)[i], target=list(self.graph.nodes)[j], weight="weight")
 
 
     def get_random_solutions(self):
@@ -23,7 +16,7 @@ class GeneticAlgo(AbstractAlgo):
 
         solutions = []
 
-        for solution_index in range(self.solutions_in_parallel):
+        for _ in range(self.solutions_in_parallel):
             nodes = list(self.graph.nodes)[1:]
             solution = [[] for i in range(self.num_vehicles)]
             while len(nodes) > 0:
@@ -42,21 +35,7 @@ class GeneticAlgo(AbstractAlgo):
             
             solutions.append(solution)
         
-        return solutions
-    
-    def get_distance_per_vehicle(self, solution):
-        # Calculate the distance for each vehicle
-        distances = 0
-        for i in range(len(solution)-1):
-            distances += nx.shortest_path_length(self.graph, source=solution[i], target=solution[i+1], weight="weight")
-        return distances
-    
-    def get_total_distance(self, solution):
-        # Calculate the total distance of the solution
-        total_distance = 0
-        for i in range(len(solution)):
-            total_distance += self.get_distance_per_vehicle(solution[i])
-        return total_distance
+        return solutions 
     
     def mutate(self, solution):
         # Mutate the solution by swapping two random nodes
@@ -71,7 +50,7 @@ class GeneticAlgo(AbstractAlgo):
                     node1 = random.choice(solution_to_mutate).copy()
                     node2 = random.choice(solution_to_mutate).copy()
                     random_index1, random_index2 = random.randint(1, len(node1)-2), random.randint(1, len(node2)-2)
-                    if node1 == node2:
+                    if node1 == node2: # only one vehicle or same vehicle
                         solution_to_mutate.remove(node1)
                         node1[random_index1], node1[random_index2] = node1[random_index2], node1[random_index1]
                         solution_to_mutate.append(node1)
@@ -86,23 +65,50 @@ class GeneticAlgo(AbstractAlgo):
         return solution
     
     def run(self):
+        start_time = time.time()
         # Run the genetic algorithm
         # Generate a random solution
         solutions = self.get_random_solutions()
-        self.distance_history = [] #TODO
-        
-        
-        for i in range(self.max_iterations):
+        self.distance_history = []
+        similar_results_count = 0
+        for iteration in range(1, self.max_iterations + 1):
+            distances = []
+            standard_deviations = []
             # Sort solutions by their total distance
-            solutions = sorted(solutions, key=lambda s: self.get_total_distance(s))
-            solutions = solutions[:len(solutions)//2] 
-            solutions_distances = [self.get_total_distance(s) for s in solutions]
-            self.distance_history.append(solutions_distances[0])
+            for solution in solutions:
+                total_distance = 0
+                vehicle_distances = []
+                for idx, vehicle in enumerate(solution): 
+                    vehicle_distance = 0
+                    for i in range(len(vehicle) - 1):
+                        vehicle_distance += self.graph[vehicle[i]][vehicle[i + 1]]["weight"]
+                        vehicle_distances.append(vehicle_distance)
+                    total_distance += vehicle_distance
+                distances.append(total_distance)
+                standard_deviations.append(np.std(vehicle_distances))
+
+            scores = [distances[i] + standard_deviations[i] for i in range(len(distances))]
+            sorted_indices = np.argsort(scores)
+            solutions = [solutions[i] for i in sorted_indices[:len(solutions) // 2]] #get half (the betters)
+            self.distance_history.append(distances[sorted_indices[0]])
+            if len(self.distance_history) > 1 and self.distance_history[-1] == self.distance_history[-2]:
+                similar_results_count += 1
+            else:
+                similar_results_count = 0
+
+            if similar_results_count >= self.convergence_threshold and iteration > self.min_iterations:
+                break
 
             # Create new solutions by combining the best solutions
             solutions = self.mutate(solutions)
 
-        self.paths = sorted(solutions, key=lambda s: self.get_total_distance(s))[0]
-        self.distance = self.get_total_distance(self.paths)
-        self.iterations_needed = -1 #TODO
-        self.total_interations_realized = 100
+        self.paths = solutions[0]
+        self.distance = self.distance_history[-1]
+        self.iterations_needed = iteration - similar_results_count
+        self.total_interations_realized = iteration
+        self.distance_per_vehicles = [sum(self.graph[vehicle[i]][vehicle[i + 1]]["weight"] for i in range(len(vehicle) - 1)) for vehicle in self.paths]
+        self.distance_average_per_vehicles = np.mean(self.distance_per_vehicles)
+        self.distance_standard_deviation_per_vehicles = np.std(self.distance_per_vehicles)
+        self.execution_time = time.time() - start_time
+
+
