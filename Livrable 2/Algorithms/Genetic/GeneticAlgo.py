7d63c3400a12111d070914e11967e2a7287118db
BDF172 <bdf172@bdfhome.local>
2025-04-28 17:46:16 +0200

    Genetic algorithm finished

--- Livrable 2/Algorithms/Genetic/GeneticAlgo.py
+++ Livrable 2/Algorithms/Genetic/GeneticAlgo.py
@@ -4,7 +4,7 @@ import networkx as nx
 from Algorithms.AbstractAlgo import AbstractAlgo
 
 class GeneticAlgo(AbstractAlgo):
-    def __init__(self, graph, name = None, num_vehicles = 1, solutions_in_parallel = 10, min_iterations = 0, max_iterations = 100, convergence_threshold = 5):
+    def __init__(self, graph, name = None, num_vehicles = 5, solutions_in_parallel = 10, min_iterations = 0, max_iterations = 100, convergence_threshold = 5):
         super().__init__(graph=graph, name=name, num_vehicles=num_vehicles, min_iterations=min_iterations, max_iterations=max_iterations, convergence_threshold=convergence_threshold)
         self.solutions_in_parallel = solutions_in_parallel
         self.get_distance_matrix()
@@ -48,11 +48,8 @@ class GeneticAlgo(AbstractAlgo):
     def get_distance_per_vehicle(self, solution):
         # Calculate the distance for each vehicle
         distances = 0
-        for i in range(len(solution)):
-            for j in range(len(solution[i])):
-                if j == len(solution[i]) - 1:
-                    break
-                distances += nx.shortest_path_length(self.graph, source=solution[i][j], target=solution[i][j+1], weight="weight")
+        for i in range(len(solution)-1):
+            distances += nx.shortest_path_length(self.graph, source=solution[i], target=solution[i+1], weight="weight")
         return distances
     
     def get_total_distance(self, solution):
@@ -71,16 +68,22 @@ class GeneticAlgo(AbstractAlgo):
             for i in range(len(solution)):
                 if random.random() < mutation_rate:
                     # Swap two random nodes
-                    node1 = random.choice(solution[i]).copy()
-                    node2 = random.choice(solution[i]).copy()
+                    solution_to_mutate = solution[i].copy()
+                    node1 = random.choice(solution_to_mutate).copy()
+                    node2 = random.choice(solution_to_mutate).copy()
                     random_index1, random_index2 = random.randint(1, len(node1)-2), random.randint(1, len(node2)-2)
                     if node1 == node2:
+                        solution_to_mutate.remove(node1)
                         node1[random_index1], node1[random_index2] = node1[random_index2], node1[random_index1]
-                        solution.append([node1])
+                        solution_to_mutate.append(node1)
+                        solution.append(solution_to_mutate)
                     else:
+                        solution_to_mutate.remove(node1)
+                        solution_to_mutate.remove(node2)
                         node1[random_index1], node2[random_index2] = node2[random_index2], node1[random_index1]
-                        if(random.random() < 0.5): solution.append([node1])
-                        else: solution.append([node2])
+                        solution_to_mutate.append(node1)
+                        solution_to_mutate.append(node2)
+                        solution.append(solution_to_mutate)
         return solution
     
     def run(self):
@@ -102,3 +105,6 @@ class GeneticAlgo(AbstractAlgo):
 
         paths = sorted(solutions, key=lambda s: self.get_total_distance(s))[0]
         self.paths = sorted(solutions, key=lambda s: self.get_total_distance(s))[0]
+        self.distance = self.get_total_distance(self.paths)
+        self.iterations_needed = -1 #TODO
+        self.total_interations_realized = 100
